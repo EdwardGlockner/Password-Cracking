79f73d092aaaccd344d09b7a2abd5a3dc85c30d3
Edward Glöckner <101050443+EdwardGlockner@users.noreply.github.com>
2023-05-07 02:14:19 +0200

    adding dict and fixing argument parsing

--- Makefile
+++ Makefile
@@ -3,10 +3,12 @@ SRCDIR := ./source
 INCDIR := ./include
 OBJDIR := ./output
 
-pw_attack: $(OBJDIR)/dictionary_attack.o $(OBJDIR)/brute_force.o $(OBJDIR)/rule_based.o $(OBJDIR)/password_comparison.o $(OBJDIR)/main.o
+pw_attack: $(OBJDIR)/dictionary_attack.o $(OBJDIR)/brute_force.o \
+	$(OBJDIR)/rule_based.o $(OBJDIR)/password_comparison.o $(OBJDIR)/main.o
 	c++ $(CXXFLAGS) $^ -o $@
 
-$(OBJDIR)/dictionary_attack.o: $(SRCDIR)/dictionary_attack.cpp $(INCDIR)/dictionary_attack.h
+$(OBJDIR)/dictionary_attack.o: $(SRCDIR)/dictionary_attack.cpp \
+	$(INCDIR)/dictionary_attack.h
 	c++ $(CXXFLAGS) -c $< -o $@
 
 $(OBJDIR)/brute_force.o: $(SRCDIR)/brute_force.cpp $(INCDIR)/brute_force.h
@@ -15,7 +17,8 @@ $(OBJDIR)/brute_force.o: $(SRCDIR)/brute_force.cpp $(INCDIR)/brute_force.h
 $(OBJDIR)/rule_based.o: $(SRCDIR)/rule_based.cpp $(INCDIR)/rule_based.h
 	c++ $(CXXFLAGS) -c $< -o $@
 
-$(OBJDIR)/password_comparison.o: $(SRCDIR)/password_comparison.cpp $(INCDIR)/password_comparison.h
+$(OBJDIR)/password_comparison.o: $(SRCDIR)/password_comparison.cpp \
+	$(INCDIR)/password_comparison.h
 	c++ $(CXXFLAGS) -c $< -o $@
 
 $(OBJDIR)/main.o: $(SRCDIR)/main.cpp
--- source/main.cpp
+++ source/main.cpp
@@ -1,27 +1,84 @@
 #include <iostream>
+#include <tuple>
 #include <vector>
+#include <chrono>
 
 #include "../include/brute_force.h"
 #include "../include/dictionary_attack.h"
 #include "../include/rule_based.h"
 #include "../include/password_comparison.h"
 
-int main() {
+/*
+ * should check equality after each generated password
+ * not generating all passwords then checking them all
+ */
+std::tuple<std::string, std::string> flagger(int argc, char* argv[]);
+void usage_helper();
+
+
+int main(int argc, char* argv[]) {
+    flagger(argc, argv);
     Brute_Force* BF = new Brute_Force();
     Dictionary_Attack* DA = new Dictionary_Attack();
 
     std::vector<std::string> passwords;
     std::cout << "Generating passwords..." << std::endl;
-    BF->generate_passwords("", 5, passwords);
+
+    auto start1 = std::chrono::high_resolution_clock::now();
+    BF->generate_passwords("", 4, passwords);
+    auto stop1 = std::chrono::high_resolution_clock::now();
+    auto duration1 = std::chrono::duration_cast<std::chrono::milliseconds>(stop1 - start1);
+    std::cout << "Elapsed time for password generation: " << \
+        duration1.count() << " milliseconds" <<  std::endl;
     
     std::cout << "Initializing brute force attack" << std::endl;
     // Brute force attack by trying each password until the correct one is found
-    std::string correct_password = "assku"; // The correct password to be guessed
+    std::string correct_password = "assu"; // The correct password to be guessed
+    auto start2 = std::chrono::high_resolution_clock::now();
     for (std::string password : passwords) {
         if (password == correct_password) {
             std::cout << "Password found: " << password << std::endl;
+            auto stop2 = std::chrono::high_resolution_clock::now();
+            auto duration2 = std::chrono::duration_cast<std::chrono::milliseconds>(stop2-start2);
+            std::cout << "Elapsed time for brute force attack: " << duration2.count() \
+                << " milliseconds" << std::endl;
             break;
         }
     }
     return 0;
+}
+
+std::tuple<std::string, std::string> flagger(int argc, char* argv[]) {
+    std::string run_mode;
+    std::string dirname;
+    
+    if (argc > 1) {
+        // Iterate over the flags
+
+        for (int i = 1; i < argc; i++) {
+            if (strcmp(argv[i], "-h") == 0) {
+                usage_helper();
+            }
+
+            else if (strcmp(argv[i], "-m") == 0) {
+                std::cout << "MODDE: " << argv[i+1] << std::endl;
+                run_mode = argv[i+1];
+            }
+            else if (strcmp(argv[i], "-d") == 0) {
+                std::cout << "DIRNAME: " << argv[i+1] << std::endl;
+                dirname = argv[i+1];
+            }
+        }
+    } 
+    if (run_mode.empty()) {
+        std::cout << "Run mode not selected" << std::endl;
+    }
+    if (dirname.empty()) {
+        std::cout << "Dirname not specified" << std::endl;
+    }
+    return std::make_tuple(run_mode, dirname);
+}
+
+void usage_helper() {
+
 }
